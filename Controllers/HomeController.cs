2c2e2c8b5508b1464b41cfa5df6b79bffcb5f5dc
Wellington Garcia <garciawell@gmail.com>
2019-07-09 23:53:22 -0300

    Tratando Error

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,27 +8,47 @@ using demoMVC.Models;
 
 namespace demoMVC.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
-    //teste
+    //[Route("")]
+    //[Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
-        public IActionResult Index(int id, Guid categoria)
+        //[Route("")]
+        //[Route("pagina-inicial")]
+        //[Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        //public IActionResult Index(int id, Guid categoria)
+        public IActionResult Index()
         {
-            return View();
+            var filme = new Filme
+            {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = "115",
+                Valor = 20000
+            };
+
+            return RedirectToAction("Privacy", filme);
+            //return View();
         }
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
-        public IActionResult Privacy()
+        //[Route("privacidade")]
+        //[Route("politica-de-privacidade")]
+        public IActionResult Privacy(Filme filme)
         {
-            return Content("{'nome': 'Wellington'}");
+            if (ModelState.IsValid)         
+            {            
+
+            }   
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
+        //[Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
