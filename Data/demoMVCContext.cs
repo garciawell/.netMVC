3b402f4fba0bd53304660ef0ea0a9f0691a38637
Wellington Garcia <garciawell@gmail.com>
2019-07-15 23:48:34 -0300

    Scaffold Form

---
+++ Data/demoMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoMVC.Models
+{
+    public class demoMVCContext : DbContext
+    {
+        public demoMVCContext (DbContextOptions<demoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<demoMVC.Models.Filme> Filme { get; set; }
+    }
+}
